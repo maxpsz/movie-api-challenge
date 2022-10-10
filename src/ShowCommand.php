1a864a7275e19450a86fc427a43b6b1e264d2b91
Maxi Peszano <maximiliano.peszano@lightit.io>
2022-10-10 09:13:20 -0300

    api: Create Api Call and Add Env Variables

--- src/ShowCommand.php
+++ src/ShowCommand.php
@@ -8,31 +8,55 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Helper\Table;
+use GuzzleHttp\ClientInterface;
 
 class ShowCommand extends Command
 {
+    private $client;
+
+    public function __construct(ClientInterface $client)
+    {
+        $this->client = $client;
+
+        parent::__construct();
+    }
+
     public function configure()
     {
         $this->setName('show')
             ->setDescription('Get movie info')
             ->addArgument('movieName', InputArgument::REQUIRED)
-            ->addOption('fullPlot', null, InputOption::VALUE_OPTIONAL, "Get movie's full plot", 'Hello');
+            ->addOption('fullPlot', null, InputOption::VALUE_NONE, "Get movie's full plot");
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $movieName = $input->getArgument("movieName");
-        $output->writeln("<info>Searching movie: {$movieName}</info>");
+        $fullPlot = $input->getOption("fullPlot");
 
-        $this->showMovieInfo($output);
-    }
+        $baseUrl = $_ENV["API_BASE_URL"];
+        $apiKey = $_ENV["API_KEY"];
+        $request = "{$baseUrl}/?apikey={$apiKey}";
 
-    private function showMovieInfo(OutputInterface $output)
-    {
+        if ($movieName) {
+            $request = $request . "&t={$movieName}";
+        }
+
+        if ($fullPlot) {
+            $request = $request . "&plot=full";
+        }
+
+        $response = $this->client->get($request);
+        $data = json_decode($response->getBody());
+        $keys = array_keys(json_decode($response->getBody(), true));
         $table = new Table($output);
 
-        $table->setHeaders([])
-            ->setRows([])
-            ->render();
+        $excluded_keys = ["Ratings"];
+        foreach ($keys as $key) {
+            if (in_array($key, $excluded_keys)) { continue; }
+            $table->addRow([$key, $data->$key]);
+        }
+
+        $table->render();
     }
 }
